7c2b881c7a36d773110f23accfbc9b0cf9aaffdf
tonycpmeum <tonycpmeum@gmail.com>
2025-10-31 12:34:27 +0800

    mediocre version control but better UI with tender KeyError detection

--- config_manager.py
+++ config_manager.py
@@ -2,7 +2,7 @@ import os
 import json
 from typing import Any
 
-VERSION = '1.1.1'
+VERSION = '0.0.1'
 
 class ConfigManager:
    _instance = None
@@ -23,13 +23,13 @@ class ConfigManager:
          try:
             with open(self.config_path, 'r') as f:
                self._data = json.load(f)
+               if self._data["version"] != VERSION:
+                  self.reinit_config_default()
          except (json.JSONDecodeError, IOError) as e:
             print(f"Error loading config, using defaults: {e}")
-            self._data = self.get_default_config()
-            self._save_data()
+            self.reinit_config_default()
       else:
-         self._data = self.get_default_config()
-         self._save_data()
+         self.reinit_config_default()
 
    def _save_data(self):
       try:
@@ -37,6 +37,10 @@ class ConfigManager:
             json.dump(self._data, f, indent=3)
       except IOError as e:
          print(f"Error saving config: {e}")
+
+   def reinit_config_default(self):
+      self._data = self.get_default_config()
+      self._save_data()
 
    def get_default_config(self) -> dict:
       default_config = {
@@ -66,10 +70,12 @@ class ConfigManager:
    
    @property
    def max_scripts(self) -> int: 
-      if not self._data["max_scripts"]:
-         raise ValueError("Value not found. Re-initializing config with default values.")
-         self._load_config()
-      return self._data["max_scripts"]
+      try:
+         return self._data["max_scripts"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["max_scripts"]
    @max_scripts.setter
    def max_scripts(self, value: int): 
       self._data["max_scripts"] = value
@@ -78,7 +84,12 @@ class ConfigManager:
    # =============== CLILCK SETTINGS ===============
    @property
    def click_interval(self) -> float: 
-      return self._data['click_settings']["click_interval"]
+      try:
+         return self._data['click_settings']["click_interval"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data['click_settings']["click_interval"]
    @click_interval.setter
    def click_interval(self, value: float): 
       self._data['click_settings']["click_interval"] = value
@@ -86,7 +97,12 @@ class ConfigManager:
 
    @property
    def click_button(self) -> str: 
-      return self._data['click_settings']["click_button"]
+      try:
+         return self._data['click_settings']["click_button"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data['click_settings']["click_button"]
    @click_button.setter
    def click_button(self, value: str): 
       self._data['click_settings']["click_button"] = value
@@ -95,7 +111,12 @@ class ConfigManager:
    @property
    # 1 = single, 2 = double, for compatibility with pynput controller param
    def click_type(self) -> int: 
-      return self._data['click_settings']["click_type"]
+      try:
+         return self._data['click_settings']["click_type"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data['click_settings']["click_type"]
    @click_type.setter
    def click_type(self, value: int): 
       self._data['click_settings']["click_type"] = value
@@ -104,7 +125,12 @@ class ConfigManager:
    # =============== SCRIPT SETTINGS ===============
    @property
    def script_enabled(self) -> bool: 
-      return self._data["script_settings"]["script_enabled"]
+      try:
+         return self._data["script_settings"]["script_enabled"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["script_settings"]["script_enabled"]
    @script_enabled.setter
    def script_enabled(self, value: bool): 
       self._data["script_settings"]["script_enabled"] = value
@@ -112,7 +138,12 @@ class ConfigManager:
 
    @property
    def script_selected_index(self) -> int: 
-      return self._data["script_settings"]["script_selected_index"]
+      try:
+         return self._data["script_settings"]["script_selected_index"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["script_settings"]["script_selected_index"]
    @script_selected_index.setter
    def script_selected_index(self, value: int): 
       self._data["script_settings"]["script_selected_index"] = value
@@ -121,7 +152,12 @@ class ConfigManager:
    # =============== REPEAT SETTINGS ===============
    @property
    def repeat_limited(self) -> bool: 
-      return self._data["repeat_settings"]["repeat_limited"]
+      try:
+         return self._data["repeat_settings"]["repeat_limited"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["repeat_settings"]["repeat_limited"]
    @repeat_limited.setter
    def repeat_limited(self, value: bool): 
       self._data["repeat_settings"]["repeat_limited"] = value
@@ -129,7 +165,12 @@ class ConfigManager:
 
    @property
    def repeat_count(self) -> int: 
-      return self._data["repeat_settings"]["repeat_count"]
+      try:
+         return self._data["repeat_settings"]["repeat_count"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["repeat_settings"]["repeat_count"]
    @repeat_count.setter
    def repeat_count(self, value: int): 
       self._data["repeat_settings"]["repeat_count"] = value
@@ -138,7 +179,12 @@ class ConfigManager:
    # =============== RECORDING SETTINGS ===============
    @property
    def sample_mouse_move_interval(self) -> float: 
-      return self._data["recording_settings"]["sample_mouse_move_interval"]
+      try:
+         return self._data["recording_settings"]["sample_mouse_move_interval"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["recording_settings"]["sample_mouse_move_interval"]
    @sample_mouse_move_interval.setter
    def sample_mouse_move_interval(self, value: float): 
       self._data["recording_settings"]["sample_mouse_move_interval"] = value
@@ -147,7 +193,12 @@ class ConfigManager:
    # =============== PLAYBACK SETTINGS ===============
    @property
    def playback_speed(self) -> float: 
-      return self._data["playback_settings"]["playback_speed"]
+      try:
+         return self._data["playback_settings"]["playback_speed"]
+      except KeyError as e:
+         print(f"KeyError: {e}")
+         self.reinit_config_default()
+         return self._data["playback_settings"]["playback_speed"]
    @playback_speed.setter
    def playback_speed(self, value: float): 
       self._data["playback_settings"]["playback_speed"] = value
